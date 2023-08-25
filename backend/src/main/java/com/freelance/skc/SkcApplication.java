82d589526f4b4ecb71e6e641e8e38821a18bb007
garri_manasaryan <garri.manasaryan@bostongene.com>
2023-08-26 03:59:47 +0400

    Initial setup

---
+++ backend/src/main/java/com/freelance/skc/SkcApplication.java
@@ -0,0 +1,13 @@
+package com.freelance.skc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkcApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/freelance/skc/SkcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.freelance.skc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
