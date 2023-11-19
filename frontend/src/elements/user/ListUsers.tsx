b2b178556ae6d1e8bdc7a3e1d9d219705014079e
garri_manasaryan <garri.manasaryan@bostongene.com>
2023-11-19 20:37:52 +0400

    Front base

---
+++ frontend/src/elements/user/ListUsers.tsx
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from 'react'
+import axios, { AxiosHeaders, AxiosRequestHeaders } from 'axios';
+import axiosConf from '../../axiosConf';
+import { Button } from 'antd';
+import { UserInterface } from './UserInterface';
+
+function ListUsers() {
+  
+    useEffect(() => {
+        getTasks();
+    }, [])
+  
+    const getTasks = () => {
+      axiosConf.get('/api/users')
+        .then((response) => {
+          console.log(response.data)
+        })
+    }
+
+    const createUser = () => {
+        const formValues: UserInterface = {
+            name: "csac",
+            email: "acsaa",
+            role: "ADMIN",
+            age: 4,
+            discriminator: "CLIENT",
+            timeZone: "GMT+2"
+        }
+        axiosConf.post('/api/users', formValues)
+        .then((response) => {
+          console.log(response)
+        })
+    }
+
+    return (
+        <>
+            <Button 
+                className='dropdown-field-btn' 
+                type='ghost' 
+                onClick={()=> createUser()}
+            >click me
+            </Button>
+        </>
+    )
+}
+
+export default ListUsers
---
+++ frontend/src/elements/user/UserInterface.tsx
@@ -0,0 +1,8 @@
+export interface UserInterface{
+    name: string;
+    email: string;
+    role: string;
+    age: number;
+    discriminator: string;
+    timeZone: string;
+}
